3c1cdb176cc959c0788f33ff880a080b532e4baa
Kanjoju Charan <kanjoju.charan@bounteous.com>
2026-03-10 18:30:15 +0530

    add auth middleware for protected routes

---
+++ backend/src/routes/userRoutes.ts
@@ -0,0 +1,19 @@
+import express from "express"
+import { authMiddleware } from "../middleware/authMiddleware"
+
+const router = express.Router()
+
+router.get(
+  "/profile",
+  authMiddleware,
+  (req: any, res) => {
+
+    res.json({
+      message: "Protected route accessed",
+      userId: req.userId
+    })
+
+  }
+)
+
+export default router
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import userRoutes from "./routes/userRoutes"
 import authRoutes from "./auth/authRoutes"
 
 dotenv.config()
@@ -9,7 +10,9 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
 app.use("/api/auth", authRoutes)
+app.use("/api/user", userRoutes)
 
 app.get("/", (req, res) => {
   res.send("Ecommerce API Running")
